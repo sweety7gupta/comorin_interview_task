5f027c137f7c17666474bd0e7657e78ae066b587
sweety shaw <shawsweety27@gmail.com>
2020-12-16 22:16:51 +0530

    added filter by employee name

--- src/EmployeeTable.js
+++ src/EmployeeTable.js
@@ -4,43 +4,76 @@ import EmployeeRow from './EmployeeRow';
 
 export default class EmployeeTable extends React.Component {
     state = {
-        employees: this.props.employees
+        employees: this.props.employees,
+        employeesToRender: this.props.employees,
+        searchText: '',
+    };
+
+    getEmployeesToRender = () => {
+        const { employees, searchText } = this.state;
+        const text = searchText && searchText.toLowerCase();
+
+        const filteredEmployees = employees.filter((emp) =>
+            emp.name.toLowerCase().indexOf(text) > -1
+        );
+
+        return filteredEmployees;
     };
 
     handleEmployeeStatusChange = (employee) => {
         const { employees } = this.state;
         const index = employees.findIndex((emp) => emp.id === employee.id);
-        const empsBeforeIndex = this.state.employees.slice(0, index);
-        const empsAfterIndex = this.state.employees.slice(index + 1);
-        const empAtIndex = this.state.employees[index];
-        const empStatusAtIndex = this.state.employees[index].active;
+        const empsBeforeIndex = employees.slice(0, index);
+        const empsAfterIndex = employees.slice(index + 1);
+        const empAtIndex = employees[index];
+        const empStatusAtIndex = employees[index].active;
+
+        const updatedEmployees = [
+            ...empsBeforeIndex,
+            {
+                ...empAtIndex,
+                active: !empStatusAtIndex
+            },
+            ...empsAfterIndex,
+        ];
 
         this.setState({
-            employees: [
-                ...empsBeforeIndex,
-                {
-                    ...empAtIndex,
-                    active: !empStatusAtIndex
-                },
-                ...empsAfterIndex,
-            ]
+            employees: updatedEmployees,
+        });
+    };
+
+    handleSearchTextChange = (event) => {
+        const { value } = event.target;
+        const searchText = value.trim();
+
+        this.setState({
+            searchText: value
         });
     };
 
     render() {
-        const { employees } = this.state;
+        const { searchText } = this.state;
 
         return (
-            <div className="employee-table">
-                <EmployeeHeader />
+            <React.Fragment>
+                <input
+                    type="text"
+                    placeholder="Search by employee name"
+                    value={searchText}
+                    onChange={this.handleSearchTextChange}
+                />
 
-                {employees.map(employee => (
-                    <EmployeeRow
-                        employee={employee}
-                        onEmployeeStatusChange={this.handleEmployeeStatusChange}
-                    />
-                ))}
-            </div>
+                <div className="employee-table">
+                    <EmployeeHeader />
+
+                    {this.getEmployeesToRender().map(employee => (
+                        <EmployeeRow
+                            employee={employee}
+                            onEmployeeStatusChange={this.handleEmployeeStatusChange}
+                        />
+                    ))}
+                </div>
+            </React.Fragment>
         )
     }
 }
